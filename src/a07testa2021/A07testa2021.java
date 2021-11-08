920facb083c78ad17f931903c195fe01d3775e4d
manu_ <manu_@MANU-LAPTOP>
2021-11-08 16:15:00 -0500

    Initial Commit

---
+++ src/a07testa2021/A07testa2021.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package a07testa2021;
+
+/**
+ *
+ * @author manu_
+ */
+public class A07testa2021 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Bonjour à tous");
+        
+    }
+    
+}
